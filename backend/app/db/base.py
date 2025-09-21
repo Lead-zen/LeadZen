7372e8128d330f5cefc49031f558a611fdc9f2c1
Sushma Sharma <154212342+SumiSushmA@users.noreply.github.com>
2025-09-21 11:22:29 +0545

    removed

--- backend/app/db/base.py
+++
@@ -1,5 +0,0 @@
-from sqlmodel import SQLModel
-from app.models.users import User
-from app.models.timestamp import TimestampMixin
-from app.models.refresh_token import RefreshToken
-from app.models.lead import Lead
