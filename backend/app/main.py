c9015cac9dd3fd72044d209e0dd2b1ef967f51ae
Aman Shakya <138234962+AmanShakya28515@users.noreply.github.com>
2025-10-13 19:23:42 +0545

    Updated allowed origins , adding domain

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,7 +34,7 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://evecta.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
